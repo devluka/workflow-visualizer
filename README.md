e00dbd5ae742bcfe0354b59fa8f0dc7846e87da7
esabate <eudald.sabate@est.fib.upc.edu>
2021-02-01 18:12:50 +0100

    Updated display_wf_info.py to define previous positions as global.

--- README.md
+++ README.md
@@ -2,11 +2,26 @@
   Render the Offline logs of the CLASS workflows into the processed video
 
 
-## Tif file path
+### pip3 dependencies
+```bash
+pip3 install numpy
+pip3 install opencv-python
+pip3 install wheel
+pip3 install pandas
+```
+### geolocation_map file path
 ```bash
 /home/nvidia/masa_map.tif
 ```
-## Usage
-
+### How to run it
 ```python
-python3 rendervideo.py
+python display_wf_info.py <video_input> <Projection_Matrix> <geolocation_map> <log_file_at_edge> [<log_file_at_TP>] (optional) [<log_file_at_CD>] (optional)
+```
+Agruments:
+
+* video_input : path to the video upon which the information will be printed 
+* projection_matrix : path to the projection matrix file
+* geolocation_map: path to the .tif file
+* log_file_at_edge: path to log file which includes workflow execution information
+* log_file_at_TP: path to the TP log file
+* log_file_at_CD: path to the CD log file
--- display_wf_info.py
+++ display_wf_info.py
@@ -9,6 +9,7 @@ import cv2
 import pandas as pd
 from osgeo import gdal
 
+previous_positions = 100
 
 def GPS2Pixel(lat, lon, InvProjMat, adfGeoTransform):
     x = (lon - adfGeoTransform[0]) / adfGeoTransform[1]
@@ -17,7 +18,7 @@ def GPS2Pixel(lat, lon, InvProjMat, adfGeoTransform):
     mapPixels = np.array([[[x, y]]], dtype='float32')
     imgPixels = cv2.perspectiveTransform(mapPixels, InvProjMat)
 
-    #print("At GPS2Pixel: ("+str(lat)+","+str(lon)+")->("+str(imgPixels[0][0][0])+","+str(imgPixels[0][0][1])+")")
+    # print("At GPS2Pixel: ("+str(lat)+","+str(lon)+")->("+str(imgPixels[0][0][0])+","+str(imgPixels[0][0][1])+")")
     return int(imgPixels[0][0][0]), int(imgPixels[0][0][1])
 
 def Pixel2GPS(img_x, img_y, ProjMat, adfGeoTransform):
@@ -30,7 +31,8 @@ def Pixel2GPS(img_x, img_y, ProjMat, adfGeoTransform):
     lat = adfGeoTransform[4] * x + adfGeoTransform[5] * y + adfGeoTransform[3];
     lon = adfGeoTransform[1] * x + adfGeoTransform[2] * y + adfGeoTransform[0];
 
-    #print("At Pixel2GPS: ("+str(img_x)+","+str(img_y)+")->("+str(lat)+","+str(lon)+")")
+    # print(f"agfGeo[0]: {adfGeoTransform[0]} [1] {adfGeoTransform[1]} [2] {adfGeoTransform[2]} [3] {adfGeoTransform[3]} [4] {adfGeoTransform[4]}")
+    # print("At Pixel2GPS: ("+str(img_x)+","+str(img_y)+")->("+str(lat)+","+str(lon)+")")
     return lat,lon
 
 def generate_transformation_da(ProjMat_file,tif_file):
@@ -50,8 +52,30 @@ def generate_transformation_da(ProjMat_file,tif_file):
 
     return ProjMat, InvProjMat, adfGeoTransform
 
-def display_boxes(cap, out, df, TPdf, ProjMat, InvProjMat, adfGeoTransform):
+def add_frame_pixels(positions_dict, object_id, pixels):
+    if object_id not in positions_dict.keys():
+        positions_dict[object_id] = [pixels]
+    else:
+        positions_dict[object_id].append(pixels)
 
+    if len(positions_dict[object_id]) > previous_positions:
+        positions_dict[object_id].pop(0)
+
+def print_frame_positions(positions_dict, image, spacing=20):
+    for i, (key, val) in enumerate(positions_dict.items()):
+        if len(val) > 1:
+            for i in range(len(val) - 1):
+                pixel1 = val[i]
+                pixel2 = val[i+1]
+
+                cv2.line(image, (pixel1[0], pixel1[1]), (pixel2[0]+10, pixel2[1]+10), (0,255,0), 2)
+                cv2.circle(image, pixel1, 4, (0,255,0), 3)
+
+                if (i == len(val)-2):
+                    cv2.circle(image, pixel2, 4, (0,255,0), 3)
+
+
+def display_boxes(cap, out, df, TPdf, ProjMat, InvProjMat, adfGeoTransform, tp_avail, CDdf, cd_avail):
     # Colour codes of identified objects
     # 0: (0,127,255)   Person (Orange)
     # 1: (255,0,0)     Car (Blue)
@@ -67,13 +91,18 @@ def display_boxes(cap, out, df, TPdf, ProjMat, InvProjMat, adfGeoTransform):
 
     current_frame = 0
 
+    positions_dict = {}
+
     while cap.isOpened():
         ret, frame = cap.read()
         if ret == True:
 
             # extract rows according to the current video frame number
             df_current = df.loc[df['frame'] == current_frame]
-            TPdf_current = TPdf.loc[TPdf['frame'] == current_frame]
+            if tp_avail:
+                TPdf_current = TPdf.loc[TPdf['frame'] == current_frame]
+            if cd_avail:
+                CDdf_current = CDdf.loc[CDdf['frame'] == current_frame]
 
             # Process the workflow log file 
             for _, row in df_current.iterrows():
@@ -90,6 +119,9 @@ def display_boxes(cap, out, df, TPdf, ProjMat, InvProjMat, adfGeoTransform):
                 tmp_lat,tmp_lon = Pixel2GPS( x + (w/2), y + (h/2), ProjMat, adfGeoTransform)
                 frame_pixels = GPS2Pixel(lat,lon, InvProjMat, adfGeoTransform)
 
+                add_frame_pixels(positions_dict, label, frame_pixels)
+                print_frame_positions(positions_dict, frame)
+
                 cv2.rectangle(frame, (x, y), (w + x, y + h), color, 3)
                 cv2.rectangle(frame, (x, y - 25), (x + 12 * len(label), y ), color, -1)
                 cv2.putText(frame, label, (x, y - 5), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, .6, (255, 255, 255), 2, cv2.LINE_AA)
@@ -99,27 +131,41 @@ def display_boxes(cap, out, df, TPdf, ProjMat, InvProjMat, adfGeoTransform):
 
 
             # Process the TP log file 
-            for _, row in TPdf_current.iterrows():
+            if tp_avail:
+                for _, row in TPdf_current.iterrows():
 
-                label = row['obj_id']
-                TPlat, TPlon, TPts = row['TPlat'].split(','), row['TPlon'].split(','), row['TPts'].split(',')
+                    label = row['obj_id']
+                    TPlat, TPlon, TPts = row['TPlat'].split(','), row['TPlon'].split(','), row['TPts'].split(',')
                 
-                print("TP_info: Obj_id:" + str(label) + " lat:" + str(TPlat) + " lon:" + str(TPlon))
+                    print("TP_info: Obj_id:" + str(label) + " lat:" + str(TPlat) + " lon:" + str(TPlon))
 
-                for i in range(len(TPlat)-1):
+                    for i in range(len(TPlat)-1):
 
-                    TPframe_pixels1 = GPS2Pixel(float(TPlat[i]),float(TPlon[i]), InvProjMat, adfGeoTransform) 
-                    TPframe_pixels2 = GPS2Pixel(float(TPlat[i+1]),float(TPlon[i+1]), InvProjMat, adfGeoTransform) 
-                    cv2.line(frame, (TPframe_pixels1[0], TPframe_pixels1[1]), (TPframe_pixels2[0]+10,TPframe_pixels2[1]+10), (0,0,255), 2)
-                    cv2.circle(frame, TPframe_pixels1, 10, (0,0,255), 10)
+                        TPframe_pixels1 = GPS2Pixel(float(TPlat[i]),float(TPlon[i]), InvProjMat, adfGeoTransform) 
+                        TPframe_pixels2 = GPS2Pixel(float(TPlat[i+1]),float(TPlon[i+1]), InvProjMat, adfGeoTransform) 
+                        cv2.line(frame, (TPframe_pixels1[0], TPframe_pixels1[1]), (TPframe_pixels2[0]+10,TPframe_pixels2[1]+10), (0,0,255), 2)
+                        cv2.circle(frame, TPframe_pixels1, 10, (0,0,255), 10)
 
-                    if (i == len(TPlat)-2):
-                        cv2.circle(frame, TPframe_pixels2, 10, (0,0,255), 10)
+                        if (i == len(TPlat)-2):
+                            cv2.circle(frame, TPframe_pixels2, 10, (0,0,255), 10)
+            
+            if cd_avail:
+                #Process the CD log file
+                for _, row in CDdf_current.iterrows():
+                    obj_ids_labes = row['obj_ids_collision']
+                    GPS_label = row['GPS_info_about_collision']
+
+                    GPS_coordinates = GPS_label.split(',')
+                    CDlat, CDlon = GPS_coordinates[0], GPS_coordinates[1]
+                    CDframe_pixels = GPS2Pixel(float(CDlat), float(CDlon), InvProjMat, adfGeoTransform)
+                    cv2.circle(frame, CDframe_pixels, 10, (0,0,0), 10)
+                    cv2.putText(frame, obj_ids_labes, CDframe_pixels, cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, .6, (255, 255, 255), 2, cv2.LINE_AA)
+                    print("GPS_info: Obj_ids "  + str(obj_ids_labes) + " gps_info_collision " + str(GPS_label))
 
             out.write(frame)
             cv2.imshow('Frame', frame)
             cv2.waitKey(1)
-            input()
+            #input()
 
         else:
             print("Can't receive frame (stream end?). Exiting ...")
@@ -128,17 +174,34 @@ def display_boxes(cap, out, df, TPdf, ProjMat, InvProjMat, adfGeoTransform):
         current_frame += 1
 
 def main():
-    if(len(sys.argv) != 6):
-        print("Usage: python display_wf_info.py <video_input> <Projection_Matrix> <geolocation_map> <log_file_at_edge> <log_file_at_TP>")
+    print(len(sys.argv))
+    if(len(sys.argv) != 7 and len(sys.argv) != 6 and len(sys.argv) != 5):
+        print("Usage: python display_wf_info.py <video_input> <Projection_Matrix> <geolocation_map> <log_file_at_edge> [<log_file_at_TP>] (optional) [<log_file_at_CD>] (optional)")
         exit()
 
     video_input_file = sys.argv[1]
     ProjMat_file = sys.argv[2]
     geolocMap_file = sys.argv[3]
     EdgeWFlog_file =sys.argv[4]
-    TPlog_file =sys.argv[5]
-    
-    print("Displaying log information from " + EdgeWFlog_file + " and " + TPlog_file + " on video " + video_input_file)
+    tp_info_avail = False
+    cd_info_avail = False
+    if len(sys.argv) != 5:
+        if len(sys.argv) == 6 and "tp" in sys.argv[5].lower():
+            tp_info_avail = True
+            TPlog_file =sys.argv[5]
+            print("Displaying log information from " + EdgeWFlog_file + " and " + TPlog_file + " on video " + video_input_file)
+        elif(len(sys.argv) == 6 and not tp_info_avail):
+            cd_info_avail = True
+            CDlog_file = sys.argv[5]
+            print("Displaying log information from " + EdgeWFlog_file + " and " + CDlog_file + " on video " + video_input_file)
+        else:
+            tp_info_avail = True
+            cd_info_avail = True
+            TPlog_file =sys.argv[5]
+            CDlog_file = sys.argv[6]
+            print("Displaying log information from " + EdgeWFlog_file + " , " + TPlog_file + " and " + CDlog_file + " on video " + video_input_file)
+
+    print("Displaying log information from " + EdgeWFlog_file + " on video " + video_input_file)
 
     # Video upon which the information will be printed 
     cap = cv2.VideoCapture(video_input_file)
@@ -156,18 +219,28 @@ def main():
     # Log file include workflow execution information as defined in "headers" varibale 
     headers = 'cam_id frame timestamp category lat lon geohash speed yaw obj_id x y w h'.split()
     df = pd.read_csv(EdgeWFlog_file, delimiter=' ', names=headers)
-    df = df[(df['obj_id'] == "20939_1") | (df['obj_id'] == "20939_27") | (df['obj_id'] == "20939_84")]
+    # df = df[(df['obj_id'] == "20939_1") | (df['obj_id'] == "20939_27") | (df['obj_id'] == "20939_84")]
 
-    # Log file include in the TP log file as defined in "TPheaders" varibale 
-    TPheaders = 'frame ts obj_id TPlat TPlon TPts'.split()
-    TPdf = pd.read_csv(TPlog_file, delimiter=' ', names=TPheaders)
-    TPdf = TPdf[(TPdf['obj_id'] == "20939_1") | (TPdf['obj_id'] == "20939_27") | (TPdf['obj_id'] == "20939_84")]
+    TPdf = None
+    if tp_info_avail:
+        # Log file include in the TP log file as defined in "TPheaders" varibale 
+        TPheaders = 'frame ts obj_id TPlat TPlon TPts'.split()
+        TPdf = pd.read_csv(TPlog_file, delimiter=' ', names=TPheaders)
+        TPdf = TPdf[(TPdf['obj_id'] == "20939_1") | (TPdf['obj_id'] == "20939_27") | (TPdf['obj_id'] == "20939_84")]
+    
+    CDdf = None
+    if cd_info_avail:
+        #Log file include the CD log file as defined in "CDheaders" variable
+        CDheaders = 'frame obj_ids_collision GPS_info_about_collision'.split()
+        CDdf = pd.read_csv(CDlog_file, delimiter=' ', names=CDheaders)
+        CDdf = CDdf[(CDdf['obj_ids_collision'] == "20939_1,20939_27") | (CDdf['obj_ids_collision'] == "20939_84,20939_27")]
+
 
     # Generate matrices
     ProjMat, InvProjMat, adfGeoTransform = generate_transformation_da(ProjMat_file, geolocMap_file)
 
     # Display workflow information into the video
-    display_boxes(cap, out, df, TPdf, ProjMat, InvProjMat, adfGeoTransform)
+    display_boxes(cap, out, df, TPdf, ProjMat, InvProjMat, adfGeoTransform, tp_info_avail, CDdf, cd_info_avail)
 
     cap.release()
     out.release()
